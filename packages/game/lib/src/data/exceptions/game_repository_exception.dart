3dc43b3bda861aebd7cb889d16cc12fb437a3db0
benjamin sabaron <benjamin.sabaron@gmail.com>
2025-12-18 16:56:10 +0100

    refactor(game): simplify RepositoryOperationException by removing the toString method

--- packages/game/lib/src/data/exceptions/game_repository_exception.dart
+++ packages/game/lib/src/data/exceptions/game_repository_exception.dart
@@ -1,9 +1,5 @@
-/// Exception thrown when a repository operation fails.
 class RepositoryOperationException implements Exception {
   const RepositoryOperationException(this.message);
 
   final String message;
-
-  @override
-  String toString() => 'RepositoryOperationException: $message';
 }
